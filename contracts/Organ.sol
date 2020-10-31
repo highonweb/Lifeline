0ff2e1bcd98ed9f99bf5b82e0113856c091c0673
siva@2204 <sivaraman26r@gmail.com>
2020-10-31 22:22:25 +0530

    [Fix]:Bug Organ.sol

--- contracts/Organ.sol
+++ contracts/Organ.sol
@@ -1,56 +1,66 @@
 pragma solidity >=0.5.0 <0.8.0;
-import './DonorContract.sol';
-import './Organisation.sol';
-contract Organ is DonorContract,Organisation {
-    struct organ{
+import "./DonorContract.sol";
+import "./Organisation.sol";
+
+contract Organ is DonorContract, Organisation {
+    struct organ {
         uint256 donorId;
         address orgAddress;
         bool donationStatus;
     }
-        organ[] public Eyes;
-        uint public totalEyes=0;
-        organ[] public Kidney;
-        uint public totalKidney=0;
-        organ[] public Liver;
-        uint public totalLiver=0;
-        organ[] public Heart;
-        uint public totalHeart=0;
-        organ[] public Lungs;
-        uint public totalLungs=0;
+    organ[] public Eyes;
+    uint256 public totalEyes;
+    organ[] public Kidney;
+    uint256 public totalKidney;
+    organ[] public Liver;
+    uint256 public totalLiver;
+    organ[] public Heart;
+    uint256 public totalHeart;
+    organ[] public Lungs;
+    uint256 public totalLungs;
+
+    constructor() public {
+        totalEyes = 0;
+        totalKidney = 0;
+        totalLiver = 0;
+        totalHeart = 0;
+        totalLungs = 0;
+    }
 
     modifier orgsOnly() {
-        for (uint i = 0; i < totalOrgs; i++) {
+        for (uint256 i = 0; i < totalOrgs; i++) {
             if (orgs[i].ad == msg.sender) {
                 _;
             }
         }
     }
-    function deceased(uint256 _id) orgsOnly public {
+
+    function deceased(uint256 _id) public orgsOnly {
         bool eyes;
-        bool kidney; 
+        bool kidney;
         bool liver;
         bool heart;
-        ( , , , , , , eyes,kidney,liver,heart,) = donorDetails(_id);
+        bool lung;
+        (, , , , , , eyes, kidney, liver, heart, lung) = donorDetails(_id);
         if (eyes) {
-            Eyes.push(organ(_id,msg.sender));
+            Eyes.push(organ(_id, msg.sender, false));
             totalEyes++;
         }
         if (kidney) {
-            Kidney.push(organ(_id,msg.sender));
+            Kidney.push(organ(_id, msg.sender, false));
             totalKidney++;
-        } if (liver) {
-            Liver.push(organ(_id,msg.sender));
+        }
+        if (liver) {
+            Liver.push(organ(_id, msg.sender, false));
             totalLiver++;
-        } if (heart) {
-            Heart.push(organ(_id,msg.sender));
+        }
+        if (heart) {
+            Heart.push(organ(_id, msg.sender, false));
             totalHeart++;
         }
-        if {
-            Lungs.push(organ(_id,msg.sender));
+        if (lung) {
+            Lungs.push(organ(_id, msg.sender, false));
             totalLungs++;
         }
-    
-}
-    
-
+    }
 }
