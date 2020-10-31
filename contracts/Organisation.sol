bbb98ef09e8cfe369403d284b4e0819e3a83ff29
Mohan <102119043@nitt.edu>
2020-10-31 18:48:28 +0530

    Added org address

--- contracts/Organisation.sol
+++ contracts/Organisation.sol
@@ -2,14 +2,15 @@ pragma solidity >=0.5.0 <0.8.0;
 import './Ownable.sol';
 contract Organisation is Ownable {
     struct organisation {
+        address ad;
         string name;
         string location;
         string ContactDetails;
     }
     uint public totalOrgs;
     organisation[] public orgs;
-        function addOrg (string memory _name, string memory _location, string memory _ContactDetails) onlyOwner public returns (bool) {
-            orgs.push(organisation(_name, _location, _ContactDetails));
+        function addOrg (address _address,string memory _name, string memory _location, string memory _ContactDetails) onlyOwner public returns (bool) {
+            orgs.push(organisation(_address,_name, _location, _ContactDetails));
             totalOrgs++;
         }
         function delOrg (uint _id) onlyOwner public returns (bool) {
@@ -20,8 +21,7 @@ contract Organisation is Ownable {
             }
             totalOrgs--;
         }
-        function updateOrg (uint _id, string memory _name, string memory _location, string memory _ContactDetails) onlyOwner public returns (bool) {
-            orgs[_id] = organisation(_name, _location, _ContactDetails);
+        function updateOrg (uint _id,address _address, string memory _name, string memory _location, string memory _ContactDetails) onlyOwner public returns (bool) {
+            orgs[_id] = organisation(_address,_name, _location, _ContactDetails);
         }
-
 }
