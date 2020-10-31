2aa39fbb1b0208d006e72d9e622d001eb73dd7f8
sree charan <msjh0512@gmail.com>
2020-11-01 01:52:43 +0530

    add organisation dashboard

---
+++ client/src/components/Orgdash/Orgdash.js
@@ -0,0 +1,197 @@
+import React from 'react'
+import './Orgdashstyle.css'
+import logo from './life-vest-logo3.jpg'
+import Typing from 'react-typing-animation';
+
+import { makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        minWidth:300,
+        backgroundColor: theme.palette.background.paper,
+    },
+    dividerFullWidth: {
+        margin: `5px 0 0 ${theme.spacing(2)}px`,
+    },
+    dividerInset: {
+        margin: `5px 0 0 ${theme.spacing(9)}px`,
+    },
+}));
+
+function Orgdash() {
+
+    const classes = useStyles();
+
+    return (
+
+        <div className='a'>
+
+            <div className='aa'>
+
+                <img src={logo} className='log'></img>
+
+                <div className='ab'>
+                    <Typing speed={150}>
+                        <span>Dashboard - Requirements</span>
+                    </Typing>
+                </div>
+
+            </div>
+
+            <div className='ac'>
+
+                <Typing speed={150}>
+                    <span><h1>Heart</h1></span>
+                </Typing>
+
+                <div className='lista'>
+
+                    <List className={classes.root}>
+                        <ListItem>
+                            <ListItemText primary="Age" secondary="19" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Blood Group" secondary="A+" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Name" secondary="qwwertyuj" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Contact Detail" secondary="Mobile: 9512012151" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="ID proof" secondary="Driving Liscence" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Organ Required" secondary="Heart" />
+                        </ListItem>
+
+
+                    </List>
+
+
+
+                    
+                    <List className={classes.root}>
+                        <ListItem>
+                            <ListItemText primary="Age" secondary="19" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Blood Group" secondary="A+" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Name" secondary="qwwertyuj" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Contact Detail" secondary="Mobile: 9512012151" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="ID proof" secondary="Driving Liscence" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Organ Required" secondary="Heart" />
+                        </ListItem>
+
+
+                    </List>
+
+
+
+
+
+
+                    <List className={classes.root}>
+                        <ListItem>
+                            <ListItemText primary="Age" secondary="19" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Blood Group" secondary="A+" />
+                        </ListItem>
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Name" secondary="qwwertyuj" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Contact Detail" secondary="Mobile: 9512012151" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="ID proof" secondary="Driving Liscence" />
+                        </ListItem>
+
+                        <Divider component="li" />
+                        <li>
+                        </li>
+                        <ListItem>
+                            <ListItemText primary="Organ Required" secondary="Heart" />
+                        </ListItem>
+
+
+                    </List>
+
+
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+};
+
+export default Orgdash;
